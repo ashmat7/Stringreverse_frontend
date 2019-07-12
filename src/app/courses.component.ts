0fd20446e2973a45f390e6b7e509de562e93a0f8
ASHIESH MATHEWS <ashieshmathews@gmail.com>
2019-07-12 09:32:24 +0530

    First commit

---
+++ src/app/courses.component.ts
@@ -0,0 +1,31 @@
+import { CoursesService } from './courses.service';
+import { Component } from '@angular/core';
+import { stringify } from '@angular/compiler/src/util';
+@Component(
+    {
+        selector: 'courses',
+        template: `
+        <input size="65" [value]="string" (keyup.enter)="string=$event.target.value;  onKeyUp()" />
+        <br />
+        <br />
+        <div style="text-align:center;">
+        <button (click)="onSave($event)" [style.backgroundColor]="isActive ? 'blue' : 'white' " class="btn btn-primary" [class.active]="isActive">Press Enter and Save</button>
+        </div>
+        `
+    }
+)
+export class CoursesComponent
+{   
+    string="Please input the string to be reversed here";
+    courses;
+    isActive=true;
+    onSave()
+    {
+        console.log(this.string);
+    }
+    onKeyUp()
+    {
+        console.log(this.string);
+    }
+    
+}
---
+++ src/app/courses.service.ts
@@ -0,0 +1,7 @@
+export class CoursesService
+{
+    getCourses()// a method to get the courses
+        {
+            return ["course1","course2","course3"];
+        }
+}
